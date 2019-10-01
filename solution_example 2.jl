c4f463defc09db31f4b6319c415d4dc4a1caa71b
Tim-Gaede <TimGaede@gmail.com>
2019-10-01 01:14:51 -0400

    Add files via upload

---
+++ solution_example 2.jl
@@ -0,0 +1,8 @@
+ #Single function solution
+function two_fer(name)
+    if name != nothing
+        "One for " * name * ", one for me." # returned
+    else
+        "One for you, one for me." # returned
+    end
+end 
