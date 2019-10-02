bb28229c2258befe85ba3b34c89999803a5e9ad7
Tim-Gaede <TimGaede@gmail.com>
2019-10-01 20:53:14 -0400

    Delete solution_example 2.jl

--- solution_example 2.jl
+++
@@ -1,8 +0,0 @@
-#Single function solution
-function two_fer(name)
-    if name != nothing
-        "One for " * name * ", one for me." # returned
-    else
-        "One for you, one for me." # returned
-    end
-end 
